7123bb02a6f0048d2af5543878186c74f7664f03
Ali Jayousi <alijay@alijay-mlt.client.nvidia.com>
2025-10-04 13:11:56 +0300

    Configure static files for production deployment
    - Add STATIC_ROOT for collectstatic command
    - Add WhiteNoise middleware for serving static files
    - Configure WhiteNoise storage for compression
    - Fixes 'STATIC_ROOT setting not set' build error

--- accounting_project/settings.py
+++ accounting_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add WhiteNoise for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',  # Add this for language detection
     'django.middleware.common.CommonMiddleware',
@@ -140,7 +141,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Whitenoise configuration for serving static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
